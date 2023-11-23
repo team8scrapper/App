3ab785b8b427772b811ec19d0b4d4798c0bf8f98
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r14s11.42porto.com>
2023-11-23 16:09:18 +0000

    improved script searchs, only returns url for now

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -4,23 +4,28 @@ from bs4 import BeautifulSoup
 
 # Fetch the webpage content
 def	scrapper(ean, url):
-	# Search for the product
+	# Search for the product's ean in the url
 	url = url + ean
 	response = requests.get(url)
+	if not response:
+		return None
 	soup = BeautifulSoup(response.text, "html.parser")
+	if not soup:
+		return None
 
 	# Find the product URL from the search
-	script_tag = soup.find("script", {"type": "application/ld+json"})
-	item_list_data = json.loads(script_tag.string)
-	url = item_list_data["itemListElement"][0]["url"]
-	print(item_list_data)
-	print();
-
+	script_tag = soup.find_all("script", {"type": "application/ld+json"})
+	arr = list(map(lambda x: json.loads(x.string), script_tag))
+	url = None
+	for item in arr:
+		if "@type" in item.keys() and item["@type"] == 'ItemList':
+			url = item["itemListElement"][0]["url"]
+			break
+	return url
 	# Search for the product content within the url found	
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, "html.parser")
 	script_tag = soup.find("script", {"type": "application/ld+json"})
-	print(script_tag)	
 
 	product = {}
 	if script_tag: # Read data into dictionary
