a51e337947bf7f5264e5312ce30b58442e89de25
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r5s5.42porto.com>
2023-11-24 12:04:56 +0000

    added auchan and gsoares scrappers

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,7 +1,17 @@
-from scrappers import continente
+from scrappers import continente, auchan, gsoares
+
+urls = {
+	"continente" : "https://www.continente.pt/pesquisa/?q=",
+	"auchan" : "https://www.auchan.pt/pt/pesquisa?q=",
+	"gsoares" : "https://www.garrafeirasoares.pt/pt/resultado-da-pesquisa_36.html?term=",
+	"elcorteingles" : "https://www.elcorteingles.pt/supermercado/pesquisar/?term="
+}
+
 
 def	main():
-	product = continente.scrapper("5601012004427", "https://www.continente.pt/pesquisa/?q=")
+	# product = continente.scrapper("5601012004427", urls["continente"])
+	# product = auchan.scrapper("5601012004427", urls["auchan"])
+	product = gsoares.scrapper("5601012004427", urls["gsoares"])
 	print(product)
 
 if __name__ == "__main__":
