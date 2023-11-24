151eb54012389a248a0a36ea1dd2ba33ebd7a9e5
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r14s9.42porto.com>
2023-11-24 14:49:58 +0000

    added start of garrafinhas

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,21 +1,30 @@
-from scrappers import continente, auchan, gsoares, elcorteingles, ptvineyards
+from scrappers import continente, auchan, gsoares, elcorteingles, ptvineyards, garrafinhas
 
 urls = {
 	"continente" : "https://www.continente.pt/pesquisa/?q=",
 	"auchan" : "https://www.auchan.pt/pt/pesquisa?q=",
 	"gsoares" : "https://www.garrafeirasoares.pt/pt/resultado-da-pesquisa_36.html?term=",
 	"elcorteingles" : "https://www.elcorteingles.pt/supermercado/pesquisar/?term=",
-	"portugalvineyards" : "https://www.portugalvineyards.com/pt/search?s="
+	"portugalvineyards" : "https://www.portugalvineyards.com/pt/search?s=",
+	"garrafinhas" : "https://garrafinhas.pt/?s="
 }
 
+# 5601012002515
 
 def	main():
-	# product = continente.scrapper("5601012004427", urls["continente"])
-	# product = auchan.scrapper("5601012004427", urls["auchan"])
-	# product = gsoares.scrapper("5601012004427", urls["gsoares"])
-	# product = elcorteingles.scrapper("5601012004427", urls["elcorteingles"])
-	product = ptvineyards.scrapper("5601012004427", urls["portugalvineyards"])
+	ean = "5601012002515"
+	#product = continente.scrapper(ean, urls["continente"])
+	#print(product)
+	#product = auchan.scrapper(ean, urls["auchan"])
+	#print(product)
+	product = gsoares.scrapper(ean, urls["gsoares"])
 	print(product)
+	product = elcorteingles.scrapper(ean, urls["elcorteingles"])
+	print(product)
+	product = ptvineyards.scrapper(ean, urls["portugalvineyards"])
+	print(product)
+	#product = garrafinhas.scrapper("5601012004427", urls["garrafinhas"])
+	#print(product)
 
 if __name__ == "__main__":
     main()
