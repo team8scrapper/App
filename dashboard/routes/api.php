e7ba42db894fdb5a4dca41dd9f7b73fdae8056ae
Nuno Machado <nunom4chado@gmail.com>
2023-12-12 19:03:33 +0000

    use scrapper api

--- dashboard/routes/api.php
+++ dashboard/routes/api.php
@@ -22,7 +22,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/test_store', function (Request $request) {
-    return ["status" => "ok", "content" => "from test_store"];
+    $data = [
+        "name" => $request['name'],
+        "search_url" => $request['search_url'],
+        "logo_url" => $request['logo_url'],
+        "currency" => $request['currency'],
+        "use_generic" => $request['use_generic'],
+        "base_url" => $request['base_url'],
+        "redirected" => $request['redirected'],
+        "results_classes" => $request['results_classes'],
+        "name_classes" => $request['name_classes'],
+        "price_classes" => $request['price_classes'],
+        "testing_ean" => $request['testing_ean'],
+    ];
+
+    // return $request['testing_ean'];
+    $response = Http::withHeaders([
+        'Access-Control-Allow-Origin' => '*'
+    ])->post('http://localhost:4242/test', $data);
+    
+    // $body = $response->body();
+
+    return $response->body();
 });
 
 Route::get('/test', function (Request $request) {
